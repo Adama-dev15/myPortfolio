8c0f74c3a0d9116ff9e5b7d5e4cc38273fb8c076
TRAORE ADAMA <traoreadamakoleba@gmail.com>
2024-05-03 19:00:24 +0200

    FIN ACCUEIL

--- src/components/accueil/Accueil.tsx
+++ src/components/accueil/Accueil.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import initAOS from "@/components/Animation";
 import { FaFacebookF, FaLinkedinIn, FaTiktok, FaTwitter } from "react-icons/fa";
+import Image from "next/image";
+import image_profil from "@/images/WhatsApp_Image_2024-04-08_at_13.13.02-removebg-preview.png";
 
 const typedStrings =
   "Web Designer, Web Developer, FrontEnd Developer, Apps Designer, Apps Developer, Content Creator, UI/UX Designer";
@@ -36,10 +38,13 @@ const Accueil = () => {
 
   return (
     <section className="accueil">
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-16 ">
         <div className="accueil-inner">
           <div className="content grid grid-cols-2">
-            <div className="left flex flex-col gap-5" data-aos="fade-right">
+            <div
+              className="left flex flex-col gap-5 py-28"
+              data-aos="fade-right"
+            >
               <div className="info-user font-bold text-2xl flex flex-col gap-1">
                 <p>Hello, It&apos;s Me</p>
                 <p className="text-green-500 text-3xl uppercase">
@@ -55,21 +60,39 @@ const Accueil = () => {
               </div>
               <div className="typed-text hidden">{typedStrings}</div>
               <div className="reseau flex gap-5">
-                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150  hover:shadow-lg">
                   <FaFacebookF />
                 </div>
-                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150  hover:shadow-lg">
                   <FaLinkedinIn />
                 </div>
-                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150  hover:shadow-lg">
                   <FaTiktok />
                 </div>
-                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150  hover:shadow-lg">
                   <FaTwitter />
                 </div>
               </div>
+
+              <div className="down flex items-center justify-center w-max bg-green-500 rounded-full py-2 px-4 shadow-lg hover:bg-green-400">
+                <button className="font-bold text-black">Download CV</button>
+              </div>
             </div>
-            <div className="right" data-aos="fade-left"></div>
+            <div
+              className="right  py-10 pl-24"
+              //   style={{ borderRadius: "86% 14% 86% 14% / 12% 84% 16% 88% " }}
+              data-aos="fade-left"
+            >
+              <div className="image flex items-center justify-center bg-green-500 rounded-full w-[400px] h-[400px]">
+                <Image
+                  src={image_profil}
+                  width={1000}
+                  height={400}
+                  alt="profi"
+                  className="w-[400px] h-[400px] rounded-full"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
