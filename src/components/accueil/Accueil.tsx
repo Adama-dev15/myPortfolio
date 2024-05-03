cb5ce0f35a750823f1e896dda031230a45fee32c
TRAORE ADAMA <traoreadamakoleba@gmail.com>
2024-05-03 18:05:45 +0200

    correction de l'erreur

--- src/components/accueil/Accueil.tsx
+++ src/components/accueil/Accueil.tsx
@@ -1,49 +1,75 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
-import $ from "jquery";
 import initAOS from "@/components/Animation";
+import { FaFacebookF, FaLinkedinIn, FaTiktok, FaTwitter } from "react-icons/fa";
+
+const typedStrings =
+  "Web Designer, Web Developer, FrontEnd Developer, Apps Designer, Apps Developer, Content Creator, UI/UX Designer";
 
 const Accueil = () => {
   useEffect(() => {
     initAOS();
   }, []);
 
+  const typedTextRef = useRef(null);
+
   useEffect(() => {
-    if ($(".typed-text-output").length === 1) {
-      var typed_strings = $(".typed-text").text();
-      var typed = new Typed(".typed-text-output", {
-        strings: typed_strings.split(", "),
+    const typedTextElement = typedTextRef.current;
+
+    if (typedTextElement) {
+      const options = {
+        strings: typedStrings.split(", "),
         typeSpeed: 100,
         backSpeed: 20,
         smartBackspace: false,
         loop: true,
-      });
+      };
+
+      const typed = new Typed(typedTextElement, options);
+
+      return () => {
+        typed.destroy();
+      };
     }
   }, []);
 
   return (
     <section className="accueil">
-      <div className="container mx-au px-8">
+      <div className="container mx-auto px-8">
         <div className="accueil-inner">
           <div className="content grid grid-cols-2">
-            <div className="left" data-aos="fade-right">
-              <div className="info-user font-bold text-2xl">
+            <div className="left flex flex-col gap-5" data-aos="fade-right">
+              <div className="info-user font-bold text-2xl flex flex-col gap-1">
                 <p>Hello, It&apos;s Me</p>
-                <p className="text-green-500 text-4xl uppercase">
+                <p className="text-green-500 text-3xl uppercase">
                   adama traoré
                 </p>
                 <p>
                   And I&apos;m a{" "}
-                  <span className="typed-text-output  text-green-500"></span>
+                  <span
+                    ref={typedTextRef}
+                    className="typed-text-output text-green-500"
+                  ></span>
                 </p>
-                <div className="typed-text hidden">
-                  Web Designer, Web Developer, Front End Developer, Apps
-                  Designer, Apps Developer
+              </div>
+              <div className="typed-text hidden">{typedStrings}</div>
+              <div className="reseau flex gap-5">
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                  <FaFacebookF />
+                </div>
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                  <FaLinkedinIn />
+                </div>
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                  <FaTiktok />
+                </div>
+                <div className="icon border cursor-pointer border-green-500 rounded-full w-8 h-8 flex items-center justify-center text-green-500 hover:bg-green-500 hover:text-black transition duration-150 shadow-none hover:shadow-lg">
+                  <FaTwitter />
                 </div>
               </div>
             </div>
-            <div className="rifgt" data-aos="fade-left"></div>
+            <div className="right" data-aos="fade-left"></div>
           </div>
         </div>
       </div>
